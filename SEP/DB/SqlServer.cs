f3ceb83da1e79ad85800050c5a22c8e32383cfd5
zSimpleMan <43574093+zSimpleMan@users.noreply.github.com>
2020-12-04 23:26:04 +0700

    tạo kết nối, lấy tên bảng, tên cột của bảng.

---
+++ SEP/DB/SqlServer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class SqlServer
+    {
+        public string dbName { get; set; }
+        public string dtSource { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+
+        private SqlConnection connection { get; set; }
+
+        public List<Table> tables = new List<Table>();
+
+        public SqlServer(string dbName, string dtSource, string userName, string password)
+        {
+            this.dbName = dbName;
+            this.dtSource = dtSource;
+            this.userName = userName;
+            this.password = password;
+            connection = Connector.GetConnection(this);
+        }
+
+        public void GetTableName()
+        {
+            connection.Open();
+            DataTable ltb = connection.GetSchema("Tables");
+
+            foreach(DataRow row in ltb.Rows)
+            {
+                var tb = new Table
+                {
+                    tableName = row[2].ToString()
+                };
+                tables.Add(tb);
+            }
+            connection.Close();
+        }
+
+        public void ReadColumnName()
+        {
+            connection.Open();
+            string query = "SELECT column_name as 'Column Name' FROM information_schema.columns WHERE table_name =  @tableName";
+            foreach (var table in tables)
+            {
+                SqlCommand sqlCommand;
+                sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandText = query;
+                sqlCommand.Parameters.AddWithValue("@tableName", table.tableName);
+
+                using(DbDataReader reader = sqlCommand.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            table.AddColumnName(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+            connection.Close();
+        }
+    }
+}
---
+++ SEP/DB/Table.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class Table
+    {
+        public string tableName { get; set; }
+        public List<string> lstColumnNames = new List<string>();
+        public Dictionary<string, string> typeOfColumns = new Dictionary<string, string>();
+        public List<Dictionary<string, string>> rows = new List<Dictionary<string, string>>();
+
+        public void AddColumnName(string name)
+        {
+            lstColumnNames.Add(name);
+        }
+
+        public void AddTypeOfColumn(string columnName, string type)
+        {
+            typeOfColumns.Add(columnName, type);
+        }
+
+        public void AddValueOfColumn(string columnName, string value)
+        {
+            Dictionary<string, string> row = new Dictionary<string, string>
+            {
+                { columnName, value }
+            };
+            rows.Add(row);
+        }
+    }
+}
