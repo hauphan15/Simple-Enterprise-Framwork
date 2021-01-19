5b3c304aa62ba928137ed5826ef39c69c57abfd3
zSimpleMan <43574093+zSimpleMan@users.noreply.github.com>
2021-01-19 19:15:24 +0700

    Fix image

--- SEP/Forms/BaseForm.cs
+++ SEP/Forms/BaseForm.cs
@@ -59,7 +59,14 @@ namespace Forms
                 if(column == Mytable.primaryKey)
                 {
                     PictureBox pictureBox = new PictureBox();
-                    pictureBox.Image = new Bitmap(Application.StartupPath + "\\ICON\\primary-key.png");
+                    try
+                    {
+                        pictureBox.Image = new Bitmap(Application.StartupPath + "\\ICON\\primary-key.png");
+                    }
+                    catch
+                    {
+                        pictureBox.Image = DrawText("PK", new Font("Arial", 12), Color.Yellow, Color.Gray);
+                    }
                     pictureBox.Location = new Point(525, 100 + space);
                     pictureBox.Size = new Size(20, 20);
                     pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -118,6 +125,41 @@ namespace Forms
             this.Controls.Add(hidelabel);
             this.Controls.Add(btn1);
             this.Controls.Add(btn2);
+        }
+
+        private Image DrawText(String text, Font font, Color textColor, Color backColor)
+        {
+            //first, create a dummy bitmap just to get a graphics object
+            Image img = new Bitmap(1, 1);
+            Graphics drawing = Graphics.FromImage(img);
+
+            //measure the string to see how big the image needs to be
+            SizeF textSize = drawing.MeasureString(text, font);
+
+            //free up the dummy image and old graphics object
+            img.Dispose();
+            drawing.Dispose();
+
+            //create a new image of the right size
+            img = new Bitmap((int)textSize.Width, (int)textSize.Height);
+
+            drawing = Graphics.FromImage(img);
+
+            //paint the background
+            drawing.Clear(backColor);
+
+            //create a brush for the text
+            Brush textBrush = new SolidBrush(textColor);
+
+            drawing.DrawString(text, font, textBrush, 0, 0);
+
+            drawing.Save();
+
+            textBrush.Dispose();
+            drawing.Dispose();
+
+            return img;
+
         }
         private void btn1_click(object sender, EventArgs e)
         {
